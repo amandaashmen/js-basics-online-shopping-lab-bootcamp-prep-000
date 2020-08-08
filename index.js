1f5db8f906ce8b05964c9026fdaf44ef2ed8728c
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-08 19:12:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   else {
     var cost = total()
+    cart = []
     return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`
   }
 }
