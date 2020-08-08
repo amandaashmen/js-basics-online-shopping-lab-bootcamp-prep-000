ef5504e2789e461ac6e4931a8f1ceb0e6d160770
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-08 18:47:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,36 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var newObj = {itemName: item, itemPrice: Math.floor(Math.random() * (100 +1))};
+ cart.push(newObj);
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var print = "In your cart, you have "
+  if (cart.length > 0) {
+  for (let i=0; i<cart.length; i++) {
+    print += Object.values(cart[i])[0] + " at $" + Object.values(cart[i])[1]
+    if (i+1 < cart.length) {
+      print += ', '
+      if (i+2 === cart.length) {
+        print += 'and '
+      }
+    }
+  }
+  return print + '.'
+  }
+  else {
+    return "Your shopping cart is empty."
+  }
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  for (let i = 0; i < cart.length; i++) {
+    total += Object.values(cart[i])[1]
+  }
+  return total
 }
 
 function removeFromCart(item) {
@@ -26,5 +47,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined)
+    return "Sorry, we don't have a credit card on file for you."
+  else {
+    cart = []
+    var cost = total()
+    return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`
+  }
 }
